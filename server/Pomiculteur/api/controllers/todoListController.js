644d4a272dbabf4f80956bec7855e4c6a3277ed9
Unknown <beaulielloic@gmail.com>
2018-02-05 09:55:53 +0100

    init server project

---
+++ server/Pomiculteur/api/controllers/todoListController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Task = mongoose.model('Tasks');
+
+exports.list_all_tasks = function(req, res) {
+    Task.find({}, function(err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.test = function(req, res) {
+    res.send("test");
+};
+
+exports.testParam = function(req, res) {
+    res.send("test "+req.params.param);
+};
+
