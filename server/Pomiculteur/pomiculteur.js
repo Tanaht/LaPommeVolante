60a4250d9f7571581244c3df8531f722859d205b
Unknown <beaulielloic@gmail.com>
2018-02-06 16:36:30 +0100

    add socket example

--- server/Pomiculteur/pomiculteur.js
+++ server/Pomiculteur/pomiculteur.js
@@ -1,28 +1,34 @@
 var express = require('express');
 var app = express();
+var net = require('net');
 var mongoose = require('mongoose');
-var port = 8080;
+
+var portApiRest = 8080;
+var portSocket = 5000;
 
 var Tasks = require("./api/models/todoListModel.js");
-
-var http = require('http').Server(app);
-
-var net = require('net');
-
-
 var routes = require('./api/routes/todoListRoutes'); //importing route
+
 routes(app); //register the route
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/LePommier');
 
 
+
 var serverSocket = net.createServer(function (socket) {
-    socket.write("Welcome \n");
+    console.log("new client connected to the socket");
 
-}).listen(5000);
+    socket.on('data', function (data) {
+        console.log("new data from client : "+data);
+        socket.write("ok"+data);
+    });
 
+    socket.on('end', function () {
+        console.log("client disconected");
+    });
 
+}).listen(portSocket);
 
 
 
@@ -30,6 +36,6 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-app.listen(port);
+app.listen(portApiRest);
 
 
