5dec3e7a85e8d27200e5c1dfb6b1d6c827dcbd5b
Unknown <g.brossault@hotmail.fr>
2018-02-07 17:55:21 +0100

    Setting up POST request

--- android/DroneMissions/app/src/main/java/service/CommunicationServerHelper.java
+++ android/DroneMissions/app/src/main/java/service/CommunicationServerHelper.java
@@ -5,6 +5,22 @@ package service;
  */
 
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * response format :
  *
@@ -26,27 +42,81 @@ public class CommunicationServerHelper {
      *  -> use extract response to get needed information
      */
 
-    public void throwMissionOrder(){
+    RequestQueue myRequestQueue;
 
+    public CommunicationServerHelper(Context context) {
+        myRequestQueue = Volley.newRequestQueue(context);
+    }
+
+    public void throwMissionOrder(){
+        Map<String, String> params = new HashMap<>();
+        params.put("type", "mission");
+        params.put("data", "");
+        executeRequest(params);
     }
 
     public void getReport(){
-
+        Map<String, String> params = new HashMap<>();
+        params.put("type", "report");
+        params.put("data", "");
+        executeRequest(params);
     }
 
     public void getOperationsHistory(){
-
+        Map<String, String> params = new HashMap<>();
+        params.put("type", "gethistory");
+        params.put("data", "");
+        executeRequest(params);
     }
 
     public void getMissionsInfo(){
-
+        Map<String, String> params = new HashMap<>();
+        params.put("type", "history");
+        params.put("data", "");
+        executeRequest(params);
     }
 
-    public void executeRequest(){
-        //set http params, request, post with params given
+    public void executeRequest(Map<String, String> params){
+        JSONObject jsonObj = new JSONObject(params);
+
+        String url = "http://";
+        JsonObjectRequest myJsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObj, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                extractResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //set action on error response
+            }
+        });
+
+        myRequestQueue.add(myJsonRequest);
     }
 
-    public void extractResponse(){
-        //get type and data
+    public void extractResponse(JSONObject jsonObject){
+        try {
+            String strType = jsonObject.getString("type");
+            String strData = jsonObject.getString("data");
+            switch(strType){
+                case "mission":
+                    //-> no data in result
+                    break;
+                case "report":
+                    //-> get report element
+                    break;
+                case "gethistory":
+                    //-> extract list of missions
+                    break;
+                case "history":
+                    //-> get mission information
+                    break;
+                default:
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
