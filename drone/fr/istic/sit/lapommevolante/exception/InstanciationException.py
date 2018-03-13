aed194e220fb92d99a9615422f2806cf22734900
Tanaky <charp.antoine@gmail.com>
2018-03-13 15:48:54 +0100

    Remove unused file

--- drone/fr/istic/sit/lapommevolante/exception/InstanciationException.py
+++
@@ -1,5 +0,0 @@
-
-
-class InstanciationException(Exception):
-    def __init__(self, instance, classinfo):
-        self.message = instance.__class__ + " is expected to be an instance of " + classinfo
