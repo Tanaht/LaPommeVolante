d2cc90d76d8bb334b2cfbd4cc986aed40af9aacc
Unknown <beaulielloic@gmail.com>
2018-03-13 17:44:01 +0100

    suppression des trucs inutiles

--- server/Pomiculteur/api/controllers/restApiController.js
+++ server/Pomiculteur/api/controllers/restApiController.js
@@ -6,18 +6,18 @@ var TaskCR = mongoose.model('CRTask');
 var pomDAPIController = require('./pomdAPIController.js');
 
 exports.report = function(req, res) {
-    console.log(req.params.param);
-    res.send("report "+req.params.param);
+    console.log(req.body);
+    res.send(req.body);
 };
 
 exports.mission_list = function(req, res) {
-    console.log(req.params.param);
-    res.send("mission_list "+req.params.param);
+    console.log(req.body);
+    res.send(req.body);
 };
 
 exports.mission_order = function(req, res) {
     console.log(req.body);
-    res.send("mission_order "+req.params.param);
+    pomDAPIController.sendMsgToPasserel(JSON.stringify(req.body));
 };
 
 
