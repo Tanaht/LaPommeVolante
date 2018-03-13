1ffc81d9cfe7459d1b00bb6f0ef9896457e1ee03
Unknown <beaulielloic@gmail.com>
2018-03-13 11:46:18 +0100

    correction type bdd

--- server/Pomiculteur/api/models/missionModel.js
+++ server/Pomiculteur/api/models/missionModel.js
@@ -4,32 +4,10 @@ var Schema = mongoose.Schema;
 
 var TaskSchema = new Schema({
 titre:       { type: String },
-id:       { type: int },
+id:       { type: Number },
 date:       { type: String },
-trajectory : [{ lat:Float, long:Float, alt:Float, photo:Boolean}]
+trajectory : [{ lat:Number, long:Number, alt:Number, photo:Boolean}]
 });
-
-/*
-var TaskSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Kindly enter the name of the task'
-    },
-    Created_date: {
-        type: Date,
-        default: Date.now
-    },
-    status: {
-        type: [{
-            type: String,
-            enum: ['pending', 'ongoing', 'completed']
-        }],
-        default: ['pending']
-    }
-});
-*/
-
-
 
 
 module.exports = mongoose.model('Tasks', TaskSchema);
